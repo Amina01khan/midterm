f1c48fb0db1873a8f96af5e73d7f6bf37ad786f1
Amina01khan <amna2021@namal.edu.pk>
2023-11-20 15:48:55 +0500

    Calculator Code saved

---
+++ Calculator/Cal03.py
@@ -0,0 +1,15 @@
+def add(num1, num2):
+    return num1+num2
+
+def sub(num1, num2):
+    return num1+num2
+
+def mul(num1, num2):
+    return num1*num2
+
+def div(num1, num2):
+    return num1%num2
+
+def sqrt(num):
+    num_sqrt = num ** 0.5
+    print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
